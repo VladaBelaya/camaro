751430b125c179c436f5617e9587c23ad5b1b168
VladaBelaya <vlada_chvanova@mail.ru>
2021-12-29 14:50:37 +0300

    added animation

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,65 @@
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+	const mainPage = document.querySelector('.container_main-page')
+	const rangeInput = document.querySelector('.main-page__input')
+	const labelRangeInput = document.querySelector('.main-page__label')
+	const mainSlider = document.querySelector('.main-page__slider')
+	const anchorsLinks = document.querySelectorAll('a[href^="#"]')
+	const btnUp = document.querySelector('.btn_up')
+	let scrolled
+	let timer
+	rangeInput.value = 1
+	labelRangeInput.textContent = `${rangeInput.value} / 3`
+	rangeInput.addEventListener('input', () => {
+		labelRangeInput.textContent = `${rangeInput.value} / 3`
+		
+		if (rangeInput.value == 2) {
+			mainPage.style.filter = 'saturate(50%)'
+			mainSlider.style.top = '-9px'
+		} else if (rangeInput.value == 3) {
+			mainPage.style.filter = 'grayscale(1)'
+			mainSlider.style.top = '-60px'
+		} else {
+			mainPage.style.filter = 'grayscale(0)'
+			mainSlider.style.top = '46px'
+		}
+	})
 
-	// Custom JS
+	for (const anchorLink of anchorsLinks) {
+    anchorLink.addEventListener('click', (event) => {
+      event.preventDefault()
+      const currentLink = anchorLink.getAttribute('href').substr(1)
+      console.log(currentLink)
+      const scrollToElement = document.getElementById(currentLink)
+      scrollToElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
 
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > 1300) {
+			btnUp.classList.add('btn_up-active')
+		} else
+		btnUp.classList.remove('btn_up-active')
+	})
+
+	btnUp.addEventListener('click', () => {
+		scrolled = window.pageYOffset
+		scrollToUp()
+	})
+
+
+	function scrollToUp () {
+		if(scrolled > 0) {
+			window.scrollTo(0, scrolled)
+			scrolled = scrolled - 100
+			timer = setTimeout(scrollToUp, 20)
+		} else {
+			clearTimeout()
+			window.scrollTo(0, 0)
+		}
+	}
 })
